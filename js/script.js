4231c5af40e8ad4d00b8bc3dc13e1d750f817c86
Silence Wins <budozzer@gmail.com>
2017-12-04 01:54:02 +0300

    Modals are fully working, except focusing on needed inputs in feedback

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-var modal_feedback=document.querySelector(".modal-feedback");
 var modal=document.querySelectorAll(".modal");
-var close=modal[].querySelector(".modal-close");
-var feedback_btn=document.querySelector(".btn-feedback");
+var modal_buttons = document.querySelectorAll("[id*=modal]");
 
-feedback_btn.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  if (!modal_feedback.classList.contains("modal-show")) {
-    modal_feedback.classList.add("modal-show");
-  }
-})
-
-modal_feedback.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27) {
-    if (modal_feedback.classList.contains("modal-show")) {
-      modal_feedback.classList.remove("modal-show");
+for (var i=0; i < modal.length; i++) {
+  modal_buttons[i].addEventListener("click", function(evt) {
+    evt.preventDefault();
+    document.querySelector("." + this.id).classList.add("modal-show");
+  })
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      for (var j=0; j < modal.length; j++) {
+        if (modal[j].classList.contains("modal-show")) {
+        modal[j].classList.remove("modal-show");
+        }
+      }
     }
+  })
+  modal[i].querySelector(".modal-close").addEventListener("click", function(evt) {
+    evt.preventDefault();
+    if (this.parentNode.classList.contains("modal-show")) {
+      this.parentNode.classList.remove("modal-show");
+    }
+  })
+  if (document.querySelector(".btn.return")) {
+    document.querySelector(".btn.return").addEventListener("click", function (evt) {
+      evt.preventDefault();
+      if (document.querySelector(".modal-cart").classList.contains("modal-show")) {
+        document.querySelector(".modal-cart").classList.remove("modal-show");
+      }
+    })
   }
-})
-
-close.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  if (modal_feedback.classList.contains("modal-show")) {
-    modal_feedback.classList.remove("modal-show");
-  }
-})
+}
